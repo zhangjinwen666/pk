7c756e82d84cd7384ff214b24c84f24a0887f872
haoge <809128022@qq.com>
2019-06-10 19:04:10 +0800

    上次补充

--- page/store/index.js
+++ page/store/index.js
@@ -1,5 +1,5 @@
 function t() {
-    c = 0, l = 1, u = 20, h = 0, p = "", d = "";
+  c = 0, l = 1, u = 20, h = 0, p = "", d = "";
 }
 
 function a(t) {
@@ -9,17 +9,17 @@ function a(t) {
 }
 
 function e(t) {
-    !t ? wx.navigateTo({
-        url: "shop/shopStation"
-    }) : wx.showModal({
-        title: "提示",
-        content: "您已入驻过店铺，点击确定进入店铺信息管理。",
-        success: function(t) {
-            t.confirm && wx.navigateTo({
-                url: "/page/store/shop/shopStation?query=yes"
-            });
-        }
-    });
+  !t ? wx.navigateTo({
+    url: "shop/shopStation"
+  }) : wx.showModal({
+    title: "提示",
+    content: "您已入驻过店铺，点击确定进入店铺信息管理。",
+    success: function (t) {
+      t.confirm && wx.navigateTo({
+        url: "/page/store/shop/shopStation?query=yes"
+      });
+    }
+  });
 }
 
 function o(t) {
@@ -38,51 +38,51 @@ function o(t) {
 }
 
 function n(t) {
-    t.setData({
-        loadMoreType: !0
-    });
-  w.get('shopuser/index/getshopuser', { page: l, cateid: p, search:d},function(a){
+  t.setData({
+    loadMoreType: !0
+  });
+  w.get('shopuser/index/getshopuser', { page: l, cateid: p, search: d }, function (a) {
     var e = t.data.shopList;
     h = parseInt(a.total);
-    l == 1 && ( e = []);
+    l == 1 && (e = []);
     var o = a.shop, n = [];
-    if(o.length > 0){
-      for (var s = 0; s < o.length; ++s){
+    if (o.length > 0) {
+      for (var s = 0; s < o.length; ++s) {
         s < 3 && n.push(o[s].shopname);
       }
       e = e.concat(o);
     }
     w.empty(h) ? i = "暂无更多数据" : e.length == h ? i = "暂无更多数据" : e.length < h && (i = "上啦加载更多"),
       t.setData({
-          shopList: e,
-          newShopBanner: n,
-          loadMoreType: !1,
-          loadText: i
+        shopList: e,
+        newShopBanner: n,
+        loadMoreType: !1,
+        loadText: i
       });
   });
 }
 
 function s(t) {
-  w.get('shopuser/goods/getallgooods',{page:l,goodstype:1},function(a){
+  w.get('shopuser/goods/getallgooods', { page: l, goodstype: 1 }, function (a) {
     var e = a.goods;
     t.setData({
-        productList: e,
-        loadMoreType: !1,
-        loadText: "没有更多了"
+      productList: e,
+      loadMoreType: !1,
+      loadText: "没有更多了"
     });
   })
 }
 
 function i(t) {
   var n = wx.getStorageSync('shopbanner');
-  if ('object' == typeof n && (r.getCurrentTime() - (n.time || 0)) < r.catchtime){
-      t.setData({banner:n.list});
-  }else{
-    w.get('shopuser/index/getadv','',function(e){
+  if ('object' == typeof n && (r.getCurrentTime() - (n.time || 0)) < r.catchtime) {
+    t.setData({ banner: n.list });
+  } else {
+    w.get('shopuser/index/getadv', '', function (e) {
       var l = {};
       l.time = r.getCurrentTime(), l.list = e.banner;
       wx.setStorageSync('shopbanner', l),
-        t.setData({ banner: l.list});
+        t.setData({ banner: l.list });
     });
   }
 }
@@ -90,98 +90,98 @@ function i(t) {
 var r = getApp(), c = 0, l = 1, u = 20, h = 0, p = "", d = "", w = r.requirejs('core');
 
 Page({
-    data: {
-        scrollTop: 0,
-        inputShowed: !1,
-        inputVal: "",
-        searchShowed: !1,
-        tabSelect: 0,
-        shopList: [],
-        isRefresh: !1,
-        loadMoreType: !0
-    },
-    onLoad: function(a) {
-        t(), i(this), o(this), n(this);
-    },
-    onScroll: function(t) {
-        var a = t.detail.scrollTop;
-        this.data.scrollTop = a;
-        var e = this.data.searchShowed;
-        a > 120 ? e || this.setData({
-            searchShowed: !0
-        }) : e && this.setData({
-            searchShowed: !1
-        });
-    },
-    onTouchStart: function(t) {
-        c = t.touches[0].pageY;
-    },
-    onTouchEnd: function(t) {
-        var a = t.changedTouches[0].pageY, e = this;
-        this.data.scrollTop < 10 && a - c > 50 && (this.setData({
-            isRefresh: !0
-        }), wx.startPullDownRefresh({
-            success: function(t) {
-                l = 1, n(e), setTimeout(function() {
-                    wx.stopPullDownRefresh(), e.setData({
-                        isRefresh: !1
-                    });
-                }, 1500);
-            }
-        }));
-    },
-    showInput: function() {
-        this.setData({
-            inputShowed: !0
-        });
-    },
-    hideInput: function() {
-        this.setData({
-            inputVal: "",
-            inputShowed: !1
-        }), d = "", l = 1, n(this);
-    },
-    clearInput: function() {
-        this.setData({
-            inputVal: ""
-        });
-    },
-    inputTyping: function(t) {},
-    bindSearchConfirm: function(t) {
-        var a = t.detail.value;
-        r.util.empty(a) || (d = a, l = 1, n(this));
-    },
-    onTabSelect: function(t) {
-        var a = t.currentTarget.dataset.index;
-        0 == a ? (l = 1, p = "", n(this)) : 1 == a ? (l = 1, p = this.data.catTree[0].catpath, 
-        n(this)) : 2 == a && s(this), this.setData({
-            tabSelect: a
-        });
-    },
-    onStation: function(t) {
-        a();
-    },
-    onCallClick: function(t) {
-        var a = t.currentTarget.dataset.mobie;
-        wx.makePhoneCall({
-            phoneNumber: a
-        });
-    },
-    onPreviewMap: function(t) {
-        var a = parseFloat(t.currentTarget.dataset.maplat), e = parseFloat(t.currentTarget.dataset.maplong);
-        wx.openLocation({
-            latitude: a,
-            longitude: e
-        });
-    },
-    onLoadMore: function(t) {
-        this.data.tabSelect < 2 && this.data.shopList.length < h && (l++, n(this));
-    },
-    onReady: function() {},
-    onShow: function() {},
-    onHide: function() {},
-    onUnload: function() {},
-    onPullDownRefresh: function() {},
-    onReachBottom: function() {},
-    onShareAppMessage: function() {}
+  data: {
+    scrollTop: 0,
+    inputShowed: !1,
+    inputVal: "",
+    searchShowed: !1,
+    tabSelect: 0,
+    shopList: [],
+    isRefresh: !1,
+    loadMoreType: !0
+  },
+  onLoad: function (a) {
+    t(), i(this), o(this), n(this);
+  },
+  onScroll: function (t) {
+    var a = t.detail.scrollTop;
+    this.data.scrollTop = a;
+    var e = this.data.searchShowed;
+    a > 120 ? e || this.setData({
+      searchShowed: !0
+    }) : e && this.setData({
+      searchShowed: !1
+    });
+  },
+  onTouchStart: function (t) {
+    c = t.touches[0].pageY;
+  },
+  onTouchEnd: function (t) {
+    var a = t.changedTouches[0].pageY, e = this;
+    this.data.scrollTop < 10 && a - c > 50 && (this.setData({
+      isRefresh: !0
+    }), wx.startPullDownRefresh({
+      success: function (t) {
+        l = 1, n(e), setTimeout(function () {
+          wx.stopPullDownRefresh(), e.setData({
+            isRefresh: !1
+          });
+        }, 1500);
+      }
+    }));
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: !0
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: !1
+    }), d = "", l = 1, n(this);
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (t) { },
+  bindSearchConfirm: function (t) {
+    var a = t.detail.value;
+    r.util.empty(a) || (d = a, l = 1, n(this));
+  },
+  onTabSelect: function (t) {
+    var a = t.currentTarget.dataset.index;
+    0 == a ? (l = 1, p = "", n(this)) : 1 == a ? (l = 1, p = this.data.catTree[0].catpath,
+      n(this)) : 2 == a && s(this), this.setData({
+        tabSelect: a
+      });
+  },
+  onStation: function (t) {
+    a();
+  },
+  onCallClick: function (t) {
+    var a = t.currentTarget.dataset.mobie;
+    wx.makePhoneCall({
+      phoneNumber: a
+    });
+  },
+  onPreviewMap: function (t) {
+    var a = parseFloat(t.currentTarget.dataset.maplat), e = parseFloat(t.currentTarget.dataset.maplong);
+    wx.openLocation({
+      latitude: a,
+      longitude: e
+    });
+  },
+  onLoadMore: function (t) {
+    this.data.tabSelect < 2 && this.data.shopList.length < h && (l++ , n(this));
+  },
+  onReady: function () { },
+  onShow: function () { },
+  onHide: function () { },
+  onUnload: function () { },
+  onPullDownRefresh: function () { },
+  onReachBottom: function () { },
+  onShareAppMessage: function () { }
 });
