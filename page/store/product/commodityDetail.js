5f1914e35bc5a4f86e88bbde85e495cd3ed452da
haoge <809128022@qq.com>
2019-06-10 10:24:39 +0800

    商家发布商品

--- page/store/product/commodityDetail.js
+++ page/store/product/commodityDetail.js
@@ -1,31 +1,33 @@
 function t(t) {
-    i.client.request({
-        url: "d=wxapi&c=mall_goods&m=goods_byid",
-        data: {
-            goodsid: n
-        },
-        success: function(o) {
-            for (var e = o.data.goodsinfo, n = 0; n < e.imagelist.length; ++n) e.imagelist[n] = i.client.getFileUrl(e.imagelist[n]);
-            t.setData({
-                goodsDetail: e
-            }), a(t, e.shopid);
-        }
-    });
+  w.get('shopuser/goods/goods_view',{goodsid:n},function(o){
+    if (o.error != 0){
+      console.log(33333);
+      wx.showToast({
+        title: o.message,
+        icon: "none",
+        duration: 2e3
+      });
+      setTimeout(function () {
+        wx.navigateBack({});
+      }, 1500);
+
+      return;
+    }
+    var e = o.data;
+    t.setData({
+      goodsDetail: e
+    }), a(t, e.id);
+  });
 }
 
 function a(t, a) {
-    i.client.request({
-        url: "d=wxapi&c=mall_shop&m=shop_info",
-        data: {
-            shopid: a
-        },
-        success: function(a) {
-            var o = a.data;
-            o.shopavatar_small = i.client.getAvatarUrl(o.shopavatar_small), t.setData({
-                shopDetail: o
-            });
-        }
+  w.get('shopuser/index/shop_info', { shopid:a}, function (a) {
+    var o = a.data;
+    o.shopavatar_small = o.shopavatars, t.setData({
+      shopDetail: o,
+      isshow:a.isshow
     });
+  });
 }
 
 function o(a) {
@@ -53,10 +55,12 @@ function e(a, o) {
     });
 }
 
-var i = getApp(), n = "";
+var i = getApp(), n = "", w = i.requirejs("core");
 
 Page({
-    data: {},
+    data: {
+      isshow:!1
+    },
     onLoad: function(a) {
         n = a.goodsid, t(this);
     },
--- page/user/shopManage/productManage.js
+++ page/user/shopManage/productManage.js
@@ -1,82 +1,96 @@
 function t(t) {
-    t.setData({
-        loadMoreType: !0
-    }), o.client.request({
-        url: "d=wxapi&c=mall_shop_goods&m=goods_page",
-        data: {
-            rows: 200
-        },
-        success: function(e) {
-            for (var a = e.data.rows, n = Number(e.data.total), s = 0; s < a.length; ++s) a[s].goodscover = o.client.getFileUrl(a[s].goodscover), 
-            a[s].dateline = o.util.formatDate(a[s].dateline, "yyyy-MM-dd");
-            var d = "";
-            o.util.empty(n) ? d = "暂无更多数据" : a.length == n ? d = "暂无更多数据" : a.length < n && (d = "上啦加载更多"), 
-            t.setData({
-                productList: a,
-                loadMoreType: !1,
-                loadText: d
-            });
-        },
-        fail: function(e) {
-            t.setData({
-                loadMoreType: !1,
-                loadText: "加载失败"
-            });
-        }
-    });
+  t.setData({
+    loadMoreType: !0
+  });
+  w.get('shopuser/goods/getallgooods', { page: t.data.page }, function (e) {
+    console.log(e);
+    var d = "", n = Number(e.total);
+    var a = {};
+
+
+    e.goods || (e.goods = []), e.goods.length > 0 && (a.page = t.data.page + 1, a.productList = t.data.productList.concat(e.goods));
+    o.empty(n) ? d = "暂无更多数据" : a.productList.length == n ? d = "暂无更多数据" : a.productList.length < n && (d = "上啦加载更多");
+    a.loadMoreType = !1;
+    a.loadText = d;
+    t.setData(a);
+  });
+
 }
 
 function e(t, e, a) {
-    o.client.request({
-        url: "d=wxapi&c=mall_shop_goods&m=delete",
-        data: {
-            goodsid: t
-        },
-        success: function(t) {
-            var o = a.data.productList;
-            o.splice(e, 1), a.setData({
-                productList: o
-            });
-        }
+  w.post('shopuser/goods/delete', { goodsid: t }, function (t) {
+    if (t.error != 0) {
+      wx.showToast({
+        title: t.message,
+        icon: "none",
+        duration: 2e3
+      });
+      return;
+    }
+    var o = a.data.productList;
+    o.splice(e, 1), a.setData({
+      productList: o
     });
+  });
+  return;
+  o.client.request({
+    url: "d=wxapi&c=mall_shop_goods&m=delete",
+    data: {
+      goodsid: t
+    },
+    success: function (t) {
+      var o = a.data.productList;
+      o.splice(e, 1), a.setData({
+        productList: o
+      });
+    }
+  });
 }
 
-var o = getApp();
+var o = getApp(), w = o.requirejs("core");
 
 Page({
-    data: {
-        loadMoreType: !0
-    },
-    onLoad: function(e) {
-        t(this);
-    },
-    catchEdit: function(t) {
-        var e = t.currentTarget.dataset.goodsid, o = t.currentTarget.dataset.goodstype;
-        "1" == o ? wx.navigateTo({
-            url: "releaseDevice?goodsid=" + e
-        }) : "2" == o && wx.navigateTo({
-            url: "releaseProduct?goodsid=" + e
-        });
-    },
-    catchDelete: function(t) {
-        var o = t.currentTarget.dataset.goodsid, a = t.currentTarget.dataset.index, n = t.currentTarget.dataset.goodsname, s = this;
-        wx.showModal({
-            title: "提示",
-            content: "是否删除“" + n + "”此商品？",
-            success: function(t) {
-                t.confirm && e(o, a, s);
-            }
-        });
-    },
-    onReady: function() {},
-    onShow: function() {},
-    onHide: function() {},
-    onUnload: function() {},
-    onPullDownRefresh: function() {
-        t(this), setTimeout(function() {
-            wx.stopPullDownRefresh();
-        }, 1e3);
-    },
-    onReachBottom: function() {},
-    onShareAppMessage: function() {}
+  data: {
+    loadMoreType: !0,
+    page: 1,
+    productList: []
+  },
+  onLoad: function (e) {
+    //t(this);
+  },
+  catchEdit: function (t) {
+    var e = t.currentTarget.dataset.goodsid, o = t.currentTarget.dataset.goodstype;
+    "1" == o ? wx.navigateTo({
+      url: "releaseDevice?goodsid=" + e
+    }) : "2" == o && wx.navigateTo({
+      url: "releaseProduct?goodsid=" + e
+    });
+  },
+  catchDelete: function (t) {
+    var o = t.currentTarget.dataset.goodsid, a = t.currentTarget.dataset.index, n = t.currentTarget.dataset.goodsname, s = this;
+    wx.showModal({
+      title: "提示",
+      content: "是否删除“" + n + "”此商品？",
+      success: function (t) {
+        t.confirm && e(o, a, s);
+      }
+    });
+  },
+  onReady: function () { },
+  onShow: function () {
+    this.setData({ page: 1, productList: [] });
+    t(this);
+  },
+  onHide: function () { },
+  onUnload: function () { },
+  onPullDownRefresh: function () {
+    this.setData({ page: 1, productList: [] });
+    t(this), setTimeout(function () {
+      wx.stopPullDownRefresh();
+    }, 1e3);
+  },
+  onReachBottom: function () {
+    t(this);
+  },
+  onShareAppMessage: function () { }
 });
