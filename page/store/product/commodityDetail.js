aedda2f3ebba3072801ead271fee081ae477a86f
haoge <809128022@qq.com>
2019-07-30 16:54:22 +0800

    修改bug

--- page/store/product/commodityDetail.js
+++ page/store/product/commodityDetail.js
@@ -1,7 +1,6 @@
 function t(t) {
   w.get('shopuser/goods/goods_view',{goodsid:n},function(o){
     if (o.error != 0){
-      console.log(33333);
       wx.showToast({
         title: o.message,
         icon: "none",
@@ -16,7 +15,7 @@ function t(t) {
     var e = o.data;
     t.setData({
       goodsDetail: e
-    }), a(t, e.id);
+    }), a(t, e.shopid);
   });
 }
 
@@ -25,53 +24,64 @@ function a(t, a) {
     var o = a.data;
     o.shopavatar_small = o.shopavatars, t.setData({
       shopDetail: o,
-      isshow:a.isshow
+      isshow:a.isshow,
+      lookMobile: a.lookMobile
     });
   });
 }
 
-function o(a) {
-    i.client.request({
-        url: "d=wxapi&c=mall_user_favorite&m=favorite_save",
-        data: {
-            type: "2",
-            toid: n
-        },
-        success: function(o) {
-            t(a, !0);
-        }
-    });
+function o(o) {
+  w.post('shopuser/favorite/savefavorite', { type: 2, toid: n }, function (e) {
+    console.log(e);
+    t(o);
+  });
 }
 
-function e(a, o) {
-    i.client.request({
-        url: "d=wxapi&c=mall_user_favorite&m=favorite_delete",
-        data: {
-            favoriteid: o
-        },
-        success: function(o) {
-            t(a, !0);
-        }
-    });
+function e(o, t) {
+  w.post('shopuser/favorite/cancelfavorite', { id: t }, function (t) {
+    t(o);
+  });
+
 }
 
 var i = getApp(), n = "", w = i.requirejs("core");
 
 Page({
     data: {
-      isshow:!1
+      isshow:!1,
+      lookMobile:0
     },
     onLoad: function(a) {
         n = a.goodsid, t(this);
     },
     callMobie: function(t) {
         var a = this.data.shopDetail.shoptel;
-        wx.makePhoneCall({
-            phoneNumber: a
-        });
+      var credit = this.data.lookMobile;
+      credit ? wx.showModal({
+        title: "提示",
+        content: "注意：当前查看需" + credit + "积分。",
+        success: function (ao) {
+          ao.confirm && w.get('shopuser/index/lookmobile', {}, function (e) {
+            if (e.error == 0) {
+              wx.makePhoneCall({
+                phoneNumber: a
+              });
+            } else {
+              wx.showToast({
+                title: e.message,
+                icon: 'none',
+              });
+              return;
+            }
+          });
+        }
+      }) : wx.makePhoneCall({
+        phoneNumber: a
+      });
     },
     bindCollection: function(t) {
         var a = this.data.goodsDetail.favoriteid;
+        console.log(a);
         0 == a ? o(this) : e(this, a);
     },
     bindPreviewImg: function(t) {
